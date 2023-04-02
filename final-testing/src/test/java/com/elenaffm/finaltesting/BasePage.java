073f2eff49faba21d2dfabfc1cd166033ac2054e
EFFM00 <fischietto.elena@gmail.com>
2023-04-02 13:33:22 -0300

    BasePage methods

--- final-testing/src/test/java/com/elenaffm/finaltesting/BasePage.java
+++ final-testing/src/test/java/com/elenaffm/finaltesting/BasePage.java
@@ -1,4 +1,71 @@
 package com.elenaffm.finaltesting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
 public class BasePage {
+
+    private WebDriver driver;
+
+    private WebDriverWait wait;
+
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public WebDriver chromeDriverConnection() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chomedriver/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://opencart.abstracta.us/index.php?route=common/home");
+
+        return driver;
+    }
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements(By locator) {
+        return driver.findElements(locator);
+    }
+
+    public String getText(WebElement element) {
+        return element.getText();
+    }
+
+    public String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    public void type(String inputText, By locator) {
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public Boolean isDisplayed(By locator) {
+        try {
+            return driver.findElement(locator).isDisplayed();
+        } catch(org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void visit(String url) {
+        driver.get(url);
+    }
+
 }
